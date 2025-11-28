xosnrdev/naijascript

commit 478779aef4af794b2f3c401d2654397d7205344f

chore: enable clippy in pedantic mode

src/arena/debug.rs
@@ -54,6 +54,7 @@ impl Default for Arena {
 }
 
 impl Arena {
+    #[must_use]
     pub const fn empty() -> Self {
         Self::Owned { arena: bump::Arena::empty() }
     }
@@ -111,7 +112,7 @@ impl Arena {
     pub fn vec_into_slice<'a, T: Copy>(&'a self, vec: Vec<T, &'a Self>) -> &'a [T] {
         #[allow(clippy::missing_transmute_annotations)]
         let vec = unsafe { mem::transmute(vec) };
-        self.delegate_target().vec_into_slice(vec)
+        bump::Arena::vec_into_slice(vec)
     }
 }
 

src/syntax/parser.rs
@@ -1,4 +1,4 @@
-//! The syntax parser for NaijaScript.
+//! The syntax parser for `NaijaScript`.
 
 use std::range::Range;
 use std::{mem, slice};
@@ -65,7 +65,7 @@ pub enum StringSegment<'ast> {
     Variable(&'ast str),
 }
 
-/// Represents a statement in NaijaScript.
+/// Represents a statement in `NaijaScript`.
 #[derive(Debug)]
 pub enum Stmt<'ast> {
     // Function definition: do <name>(<params>?) start ... end
@@ -133,7 +133,7 @@ pub enum Stmt<'ast> {
     },
 }
 
-/// Represents an expression in NaijaScript.
+/// Represents an expression in `NaijaScript`.
 #[derive(Debug)]
 pub enum Expr<'ast> {
     // Array indexing: expr[expr]
@@ -210,7 +210,7 @@ impl AsStr for SyntaxError {
 /// in one big notebook [`arena::Arena`].
 ///
 /// We use a hybrid approach, combining recursive-descent with Pratt parsing for
-/// expressions. See [https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html]
+/// expressions. See [<https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html>]
 pub struct Parser<'src, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>>
 where
     'src: 'ast,
@@ -276,10 +276,8 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                 | Token::Comot
                 | Token::Next
         ) {
-            match self.parse_statement() {
-                Some(stmt) => stmts.push(stmt),
-                None => self.synchronize(),
-            }
+            let stmt = self.parse_statement();
+            stmts.push(stmt);
         }
 
         let end = self.cur.span.end;
@@ -293,10 +291,8 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         let mut stmts = Vec::new_in(self.arena);
 
         while !matches!(self.cur.token, Token::EOF | Token::End) {
-            match self.parse_statement() {
-                Some(stmt) => stmts.push(stmt),
-                None => self.synchronize(),
-            }
+            let stmt = self.parse_statement();
+            stmts.push(stmt);
         }
 
         let end = self.cur.span.end;
@@ -326,7 +322,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
     }
 
     #[inline]
-    fn parse_statement(&mut self) -> Option<StmtRef<'ast>> {
+    fn parse_statement(&mut self) -> StmtRef<'ast> {
         let start = self.cur.span.start;
         match &self.cur.token {
             Token::Do => self.parse_function_def(start),
@@ -337,12 +333,12 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
             Token::Comot => {
                 self.bump(); // consume `comot`
                 let end = self.cur.span.end;
-                Some(self.alloc(Stmt::Break { span: Range::from(start..end) }))
+                self.alloc(Stmt::Break { span: Range::from(start..end) })
             }
             Token::Next => {
                 self.bump(); // consume `next`
                 let end = self.cur.span.end;
-                Some(self.alloc(Stmt::Continue { span: Range::from(start..end) }))
+                self.alloc(Stmt::Continue { span: Range::from(start..end) })
             }
             // Parse a standalone or nested block as a statement
             Token::Start => {
@@ -361,7 +357,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     );
                 }
                 let end = self.cur.span.end;
-                Some(self.alloc(Stmt::Block { block: block_ref, span: Range::from(start..end) }))
+                self.alloc(Stmt::Block { block: block_ref, span: Range::from(start..end) })
             }
             Token::Identifier(var) => {
                 let var = *var;
@@ -376,17 +372,17 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     let value_expr = self.parse_expression(0);
                     let end = self.cur.span.end;
                     match expr {
-                        Expr::Var(name, span) => Some(self.alloc(Stmt::AssignExisting {
+                        Expr::Var(name, span) => self.alloc(Stmt::AssignExisting {
                             var: name,
                             var_span: *span,
                             expr: value_expr,
                             span: Range::from(start..end),
-                        })),
-                        Expr::Index { .. } => Some(self.alloc(Stmt::AssignIndex {
+                        }),
+                        Expr::Index { .. } => self.alloc(Stmt::AssignIndex {
                             target: expr,
                             expr: value_expr,
                             span: Range::from(start..end),
-                        })),
+                        }),
                         _ => {
                             self.emit_error(
                                 Range::from(start..end),
@@ -398,12 +394,13 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                                     ),
                                 }],
                             );
-                            None
+                            let expr = self.alloc(Expr::Null(Range::default()));
+                            self.alloc(Stmt::Expression { expr, span: Range::default() })
                         }
                     }
                 } else {
                     let end = self.cur.span.end;
-                    Some(self.alloc(Stmt::Expression { expr, span: Range::from(start..end) }))
+                    self.alloc(Stmt::Expression { expr, span: Range::from(start..end) })
                 }
             }
             _ => {
@@ -415,12 +412,14 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                         message: ArenaCow::Borrowed("I dey expect statement"),
                     }],
                 );
-                None
+
+                let expr = self.alloc(Expr::Null(Range::default()));
+                self.alloc(Stmt::Expression { expr, span: Range::default() })
             }
         }
     }
 
-    fn parse_function_def(&mut self, start: usize) -> Option<StmtRef<'ast>> {
+    fn parse_function_def(&mut self, start: usize) -> StmtRef<'ast> {
         let do_span = self.cur.span;
         self.bump(); // consume `do`
 
@@ -474,7 +473,6 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     message: ArenaCow::Borrowed("I dey expect `(` after function name"),
                 }],
             );
-            return None;
         }
 
         let mut params = Vec::new_in(self.arena);
@@ -591,34 +589,26 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         let params = self.alloc(ParamList { params, param_spans });
         let end = self.cur.span.end;
 
-        Some(self.alloc(Stmt::FunctionDef {
+        self.alloc(Stmt::FunctionDef {
             name,
             name_span: Range::from(do_span.start..rparen_span.end),
             params,
             body,
             span: Range::from(start..end),
-        }))
+        })
     }
 
-    fn parse_return(&mut self, start: usize) -> Option<StmtRef<'ast>> {
+    fn parse_return(&mut self, start: usize) -> StmtRef<'ast> {
         self.bump(); // consume `return`
         let expr = match &self.cur.token {
-            Token::Number(..)
-            | Token::String(..)
-            | Token::True
-            | Token::False
-            | Token::Null
-            | Token::Identifier(..)
-            | Token::Not
-            | Token::LParen => Some(self.parse_expression(0)),
             Token::End | Token::EOF => None,
             _ => Some(self.parse_expression(0)),
         };
         let end = self.cur.span.end;
-        Some(self.alloc(Stmt::Return { expr, span: Range::from(start..end) }))
+        self.alloc(Stmt::Return { expr, span: Range::from(start..end) })
     }
 
-    fn parse_assignment(&mut self, start: usize) -> Option<StmtRef<'ast>> {
+    fn parse_assignment(&mut self, start: usize) -> StmtRef<'ast> {
         let make_span = self.cur.span;
         self.bump(); // consume `make`
 
@@ -642,7 +632,6 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                 ("_", span)
             }
             _ => {
-                let span = self.cur.span;
                 self.emit_error(
                     make_span,
                     SyntaxError::ExpectedIdentifier,
@@ -653,7 +642,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                 );
                 // To always produce a complete AST, even with errors.
                 // We insert placeholder/dummy values and continue parsing.
-                ("_", span)
+                ("_", Range::default())
             }
         };
         self.bump(); // consume variable name
@@ -667,10 +656,10 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         };
 
         let end = self.cur.span.end;
-        Some(self.alloc(Stmt::Assign { var, var_span, expr, span: Range::from(start..end) }))
+        self.alloc(Stmt::Assign { var, var_span, expr, span: Range::from(start..end) })
     }
 
-    fn parse_if(&mut self, start: usize) -> Option<StmtRef<'ast>> {
+    fn parse_if(&mut self, start: usize) -> StmtRef<'ast> {
         let if_span = self.cur.span;
         self.bump(); // consume `if to say`
 
@@ -731,7 +720,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     span: self.cur.span,
                     message: ArenaCow::Borrowed("I dey expect `end` block"),
                 }],
-            )
+            );
         }
 
         // Parse `if not so` block
@@ -770,10 +759,10 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         };
 
         let end = self.cur.span.end;
-        Some(self.alloc(Stmt::If { cond, then_b, else_b, span: Range::from(start..end) }))
+        self.alloc(Stmt::If { cond, then_b, else_b, span: Range::from(start..end) })
     }
 
-    fn parse_loop(&mut self, start: usize) -> Option<StmtRef<'ast>> {
+    fn parse_loop(&mut self, start: usize) -> StmtRef<'ast> {
         let jasi_span = self.cur.span;
         self.bump(); // consume `jasi`
 
@@ -837,7 +826,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         }
 
         let end = self.cur.span.end;
-        Some(self.alloc(Stmt::Loop { cond, body, span: Range::from(start..end) }))
+        self.alloc(Stmt::Loop { cond, body, span: Range::from(start..end) })
     }
 
     #[inline]
@@ -856,7 +845,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                 let s = self.cur.span;
                 let content = sval;
                 self.bump(); // consume string
-                self.parse_string_literal(content, s)
+                self.parse_string_literal(&content, s)
             }
             tok @ (Token::True | Token::False) => {
                 let s = self.cur.span;
@@ -902,7 +891,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                             span: self.cur.span,
                             message: ArenaCow::Borrowed("I dey expect `)`"),
                         }],
-                    )
+                    );
                 }
                 expr
             }
@@ -1117,42 +1106,46 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
 
     fn expr_span(expr: ExprRef<'ast>) -> Span {
         match expr {
-            Expr::Number(.., span) => *span,
-            Expr::String { span, .. } => *span,
-            Expr::Bool(.., span) => *span,
-            Expr::Null(span) => *span,
-            Expr::Var(.., span) => *span,
-            Expr::Binary { span, .. } => *span,
-            Expr::Unary { span, .. } => *span,
-            Expr::Call { span, .. } => *span,
-            Expr::Index { span, .. } => *span,
-            Expr::Array { span, .. } => *span,
-            Expr::Member { span, .. } => *span,
+            Expr::Number(.., span)
+            | Expr::String { span, .. }
+            | Expr::Bool(.., span)
+            | Expr::Null(span)
+            | Expr::Var(.., span)
+            | Expr::Binary { span, .. }
+            | Expr::Unary { span, .. }
+            | Expr::Call { span, .. }
+            | Expr::Index { span, .. }
+            | Expr::Array { span, .. }
+            | Expr::Member { span, .. } => *span,
         }
     }
 
-    fn parse_string_literal(&mut self, content: ArenaCow<'ast, 'src>, span: Span) -> ExprRef<'ast> {
+    fn parse_string_literal(
+        &mut self,
+        content: &ArenaCow<'ast, 'src>,
+        span: Span,
+    ) -> ExprRef<'ast> {
         let bytes = content.as_bytes();
         let len = bytes.len();
 
         // No braces = static string
         let index = memchr(b'{', bytes, 0);
         if index == len {
-            let s = self.alloc_str(&content);
+            let s = self.alloc_str(content);
             return self.alloc(Expr::String { parts: StringParts::Static(s), span });
         }
 
         let template: &'src str = match &content {
             ArenaCow::Borrowed(s) => s,
             ArenaCow::Owned(..) => {
-                let s = self.alloc_str(&content);
+                let s = self.alloc_str(content);
                 return self.alloc(Expr::String { parts: StringParts::Static(s), span });
             }
         };
 
         let segments = self.parse_template_segments(template);
         if segments.is_empty() {
-            let s = self.alloc_str(&content);
+            let s = self.alloc_str(content);
             return self.alloc(Expr::String { parts: StringParts::Static(s), span });
         }
 
@@ -1272,7 +1265,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     };
                     buffer.push(StringSegment::Literal(literal));
                     i = end;
-                    beg = i
+                    beg = i;
                 }
 
                 b'}' => {

tests/parser.rs
@@ -3,18 +3,18 @@ use naijascript::diagnostics::AsStr;
 use naijascript::syntax::parser::SyntaxError;
 
 mod common;
-use crate::common::_parse_from_src;
+use crate::common::parse_from_src;
 
 macro_rules! assert_parse {
     ($src:expr) => {{
         let arena = naijascript::arena::Arena::new(KIBI).unwrap();
-        let mut parser = _parse_from_src($src, &arena);
+        let mut parser = parse_from_src($src, &arena);
         let (.., errors) = parser.parse_program();
         assert!(errors.diagnostics.is_empty(), "Expected no errors, got {errors:?}");
     }};
     ($src:expr, $err:expr) => {{
         let arena = naijascript::arena::Arena::new(KIBI).unwrap();
-        let mut parser = _parse_from_src($src, &arena);
+        let mut parser = parse_from_src($src, &arena);
         let (.., errors) = parser.parse_program();
         assert!(!errors.diagnostics.is_empty(), "Expected errors, got none");
         assert!(

.github/workflows/ci.yml
@@ -51,9 +51,9 @@ jobs:
       - name: Run tests
         run: cargo test --all-features --all-targets
       - name: Run clippy
-        run: cargo clippy --all-features --all-targets -- --no-deps --deny warnings
+        run: cargo clippy --all-features --all-targets -- --no-deps --deny warnings -W clippy::pedantic
       - name: Check wasm
         run: |
           cd wasm
           cargo check --target wasm32-unknown-unknown
-          cargo clippy --target wasm32-unknown-unknown -- --no-deps --deny warnings
+          cargo clippy --target wasm32-unknown-unknown -- --no-deps --deny warnings -W clippy::pedantic

benches/lib.rs
@@ -22,7 +22,7 @@ fn bench_skip_comment(c: &mut Criterion) {
             let lexer = Lexer::new(src, &arena);
             let token: Vec<_> = lexer.collect();
             black_box(token);
-        })
+        });
     });
 }
 
@@ -39,7 +39,7 @@ fn bench_scan_string(c: &mut Criterion) {
             let lexer = Lexer::new(src, &arena);
             let token: Vec<_> = lexer.collect();
             black_box(token);
-        })
+        });
     });
 }
 
@@ -52,7 +52,7 @@ fn bench_parse_assignment(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
@@ -65,19 +65,19 @@ fn bench_parse_arithmetic_expr(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
 fn bench_parse_fn_def(c: &mut Criterion) {
-    let src = r#"
+    let src = r"
         do fibonacci(n) start
             if to say (n small pass 2) start
                 return n
             end
             return fibonacci(n minus 1) add fibonacci(n minus 2)
         end
-    "#;
+    ";
 
     c.bench_function("parse_function_definition", |b| {
         b.iter(|| {
@@ -86,7 +86,7 @@ fn bench_parse_fn_def(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
@@ -110,7 +110,7 @@ fn bench_parse_ctrl_flow(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
@@ -128,7 +128,7 @@ fn bench_parse_string_interpolation(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
@@ -142,7 +142,7 @@ fn bench_parse_nested_expr(c: &mut Criterion) {
             let mut parser = Parser::new(lexer, &arena);
             let (root, error) = parser.parse_program();
             black_box((root, error));
-        })
+        });
     });
 }
 
@@ -154,7 +154,7 @@ fn bench_string_slice(c: &mut Criterion) {
             let arena = scratch_arena(None);
             let s = builtins::StringBuiltin::slice(src, 0.0, 5.0, &arena);
             black_box(s);
-        })
+        });
     });
 }
 
@@ -165,7 +165,7 @@ fn bench_two_way(c: &mut Criterion) {
         b.iter(|| {
             let index = builtins::find(src, "uv");
             black_box(index);
-        })
+        });
     });
 }
 

src/arena/bump.rs
@@ -169,7 +169,7 @@ impl Arena {
         unsafe { slice::from_raw_parts_mut(ptr.cast().as_ptr(), count) }
     }
 
-    pub fn vec_into_slice<'a, T: Copy>(&'a self, mut vec: Vec<T, &'a Self>) -> &'a [T] {
+    pub fn vec_into_slice<T: Copy>(mut vec: Vec<T, &Self>) -> &[T] {
         if vec.is_empty() {
             return &[];
         }

src/arena/cow.rs
@@ -15,12 +15,14 @@ pub enum ArenaCow<'arena, 'src> {
 impl<'arena, 'src> ArenaCow<'arena, 'src> {
     /// Creates a new borrowed `ArenaCow` from a string reference.
     #[inline]
+    #[must_use]
     pub const fn borrowed(s: &'src str) -> Self {
         ArenaCow::Borrowed(s)
     }
 
     /// Creates a new owned [`ArenaCow`] from an `ArenaString`.
     #[inline]
+    #[must_use]
     pub const fn owned(s: ArenaString<'arena>) -> Self {
         ArenaCow::Owned(s)
     }
@@ -33,12 +35,14 @@ impl<'arena, 'src> ArenaCow<'arena, 'src> {
 
     /// Returns `true` if the data is borrowed.
     #[inline]
+    #[must_use]
     pub const fn is_borrowed(&self) -> bool {
         matches!(self, ArenaCow::Borrowed(..))
     }
 
     /// Returns `true` if the data is owned.
     #[inline]
+    #[must_use]
     pub const fn is_owned(&self) -> bool {
         matches!(self, ArenaCow::Owned(..))
     }
@@ -58,7 +62,7 @@ impl<'arena, 'src> ArenaCow<'arena, 'src> {
                 *self = ArenaCow::Owned(ArenaString::from_str(arena, s));
                 match self {
                     ArenaCow::Owned(s) => s,
-                    _ => unreachable!(),
+                    ArenaCow::Borrowed(_) => unreachable!(),
                 }
             }
             ArenaCow::Owned(s) => s,
@@ -67,18 +71,20 @@ impl<'arena, 'src> ArenaCow<'arena, 'src> {
 
     /// Returns the length of the string in bytes.
     #[inline]
+    #[must_use]
     pub fn len(&self) -> usize {
         self.as_ref().len()
     }
 
     /// Returns `true` if the string is empty.
     #[inline]
+    #[must_use]
     pub fn is_empty(&self) -> bool {
         self.as_ref().is_empty()
     }
 }
 
-impl<'arena, 'src> AsRef<str> for ArenaCow<'arena, 'src> {
+impl AsRef<str> for ArenaCow<'_, '_> {
     #[inline]
     fn as_ref(&self) -> &str {
         match self {
@@ -88,7 +94,7 @@ impl<'arena, 'src> AsRef<str> for ArenaCow<'arena, 'src> {
     }
 }
 
-impl<'arena, 'src> Deref for ArenaCow<'arena, 'src> {
+impl Deref for ArenaCow<'_, '_> {
     type Target = str;
 
     #[inline]
@@ -97,58 +103,58 @@ impl<'arena, 'src> Deref for ArenaCow<'arena, 'src> {
     }
 }
 
-impl<'arena, 'src> fmt::Display for ArenaCow<'arena, 'src> {
+impl fmt::Display for ArenaCow<'_, '_> {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(self.as_ref(), f)
     }
 }
 
-impl<'arena, 'src> PartialEq for ArenaCow<'arena, 'src> {
+impl PartialEq for ArenaCow<'_, '_> {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
         self.as_ref() == other.as_ref()
     }
 }
 
-impl<'arena, 'src> PartialEq<str> for ArenaCow<'arena, 'src> {
+impl PartialEq<str> for ArenaCow<'_, '_> {
     #[inline]
     fn eq(&self, other: &str) -> bool {
         self.as_ref() == other
     }
 }
 
-impl<'arena, 'src> PartialEq<&str> for ArenaCow<'arena, 'src> {
+impl PartialEq<&str> for ArenaCow<'_, '_> {
     #[inline]
     fn eq(&self, other: &&str) -> bool {
         self.as_ref() == *other
     }
 }
 
-impl<'arena, 'src> PartialEq<String> for ArenaCow<'arena, 'src> {
+impl PartialEq<String> for ArenaCow<'_, '_> {
     #[inline]
     fn eq(&self, other: &String) -> bool {
         self.as_ref() == other.as_str()
     }
 }
 
-impl<'arena, 'src> Eq for ArenaCow<'arena, 'src> {}
+impl Eq for ArenaCow<'_, '_> {}
 
-impl<'arena, 'src> PartialOrd for ArenaCow<'arena, 'src> {
+impl PartialOrd for ArenaCow<'_, '_> {
     #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<'arena, 'src> Ord for ArenaCow<'arena, 'src> {
+impl Ord for ArenaCow<'_, '_> {
     #[inline]
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.as_ref().cmp(other.as_ref())
     }
 }
 
-impl<'arena, 'src> From<&'src str> for ArenaCow<'arena, 'src> {
+impl<'src> From<&'src str> for ArenaCow<'_, 'src> {
     #[inline]
     fn from(s: &'src str) -> Self {
         ArenaCow::Borrowed(s)

src/arena/scratch.rs
@@ -39,6 +39,7 @@ pub fn init(capacity: usize) -> Result<(), u32> {
 /// # Safety
 ///
 /// If your function takes an [`Arena`] argument, you **MUST** pass it to `scratch_arena` as `Some(&arena)`.
+#[must_use]
 pub fn scratch_arena(conflict: Option<&Arena>) -> ScratchArena<'static> {
     unsafe {
         #[cfg(test)]
@@ -49,9 +50,9 @@ pub fn scratch_arena(conflict: Option<&Arena>) -> ScratchArena<'static> {
         }
 
         #[cfg(debug_assertions)]
-        let conflict = conflict.map(|a| a.delegate_target_unchecked());
+        let conflict = conflict.map(super::debug::Arena::delegate_target_unchecked);
 
-        let index = opt_ptr_eq(conflict, Some(&S_SCRATCH[0])) as usize;
+        let index = usize::from(opt_ptr_eq(conflict, Some(&S_SCRATCH[0])));
         let arena = &S_SCRATCH[index];
         ScratchArena::new(arena)
     }
@@ -115,13 +116,11 @@ impl Deref for ScratchArena<'_> {
 
 /// Surprisingly, there's no way in Rust to do a `ptr::eq` on `Option<&T>`.
 /// Uses `unsafe` so that the debug performance isn't too bad.
-#[inline(always)]
 #[allow(clippy::ptr_eq)]
 pub fn opt_ptr_eq<T>(a: Option<&T>, b: Option<&T>) -> bool {
     opt_ptr(a) == opt_ptr(b)
 }
 
-#[inline(always)]
 #[allow(clippy::ptr_eq)]
 fn opt_ptr<T>(a: Option<&T>) -> *const T {
     unsafe { mem::transmute(a) }

commit 734706fb9a31aaa3274cc970b07d66ecd77ecd6e

fix: benchmark regression in 478779a

src/arena/debug.rs
@@ -109,7 +109,8 @@ impl Arena {
         self.delegate_target().alloc_uninit_slice(count)
     }
 
-    pub fn vec_into_slice<'a, T: Copy>(&'a self, vec: Vec<T, &'a Self>) -> &'a [T] {
+    #[must_use]
+    pub fn vec_into_slice<T: Copy>(vec: Vec<T, &Self>) -> &[T] {
         #[allow(clippy::missing_transmute_annotations)]
         let vec = unsafe { mem::transmute(vec) };
         bump::Arena::vec_into_slice(vec)

src/syntax/parser.rs
@@ -281,7 +281,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         }
 
         let end = self.cur.span.end;
-        let stmts = self.arena.vec_into_slice(stmts);
+        let stmts = Arena::vec_into_slice(stmts);
         self.alloc(Block { stmts, span: Range::from(start..end) })
     }
 
@@ -296,7 +296,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
         }
 
         let end = self.cur.span.end;
-        let stmts = self.arena.vec_into_slice(stmts);
+        let stmts = Arena::vec_into_slice(stmts);
         self.alloc(Block { stmts, span: Range::from(start..end) })
     }
 
@@ -583,8 +583,8 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
             );
         }
 
-        let params = self.arena.vec_into_slice(params);
-        let param_spans = self.arena.vec_into_slice(param_spans);
+        let params = Arena::vec_into_slice(params);
+        let param_spans = Arena::vec_into_slice(param_spans);
 
         let params = self.alloc(ParamList { params, param_spans });
         let end = self.cur.span.end;
@@ -929,7 +929,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                     self.cur.span.end
                 };
 
-                let elements = self.arena.vec_into_slice(elements);
+                let elements = Arena::vec_into_slice(elements);
                 self.alloc(Expr::Array { elements, span: Range::from(start..end) })
             }
             _ => {
@@ -1038,7 +1038,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
                         }],
                     );
                 }
-                let args = self.arena.vec_into_slice(args);
+                let args = Arena::vec_into_slice(args);
                 let args = self.alloc(ArgList { args });
                 let end = self.cur.span.end;
                 lhs = self.alloc(Expr::Call {
@@ -1149,7 +1149,7 @@ impl<'src: 'ast, 'ast, I: Iterator<Item = SpannedToken<'ast, 'src>>> Parser<'src
             return self.alloc(Expr::String { parts: StringParts::Static(s), span });
         }
 
-        let s = self.arena.vec_into_slice(segments);
+        let s = Arena::vec_into_slice(segments);
         self.alloc(Expr::String { parts: StringParts::Interpolated(s), span })
     }
 